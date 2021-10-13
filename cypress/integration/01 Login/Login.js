f1812e2a5ce1c77a58aede026fdf98bc4e79e353
EDGAR <edgarresendiz@hotmail.com>
2021-10-13 18:32:49 -0500

    IntegraS3 - Login test

--- cypress/integration/01 Login/Login.js
+++ cypress/integration/01 Login/Login.js
@@ -8,7 +8,17 @@ describe("Login", () =>{
    
     it("Usuario valido", () =>{
         let tiempo = 1000
+        cy.login("https://integras3qa.lennken.com/Default.aspx", "0000333", "$0000333$")
+    })
+
+    it("Usuario no valido", () =>{
+        let tiempo = 1000
         cy.login("https://integras3qa.lennken.com/Default.aspx", "0000333", "$000033$")
+    })
+
+    it.only("Usuario no valido con muchos caracteres", () =>{
+        let tiempo = 1000
+        cy.login("https://integras3qa.lennken.com/Default.aspx", "asdfasdfasdfasdfasdfasdfasdfasfd", "asdfasdfasdfasdfasdfasdfasdfasdfasdf")
     })
 
     it.only("Cambio de contraseña", () =>{
@@ -20,7 +30,7 @@ describe("Login", () =>{
 
         //buscar por xpath (recuerda que se tiene que agregar la libreria require('cypress-xpath') )
         cy.frameLoaded('#dialog-body')
-        cy.iframe().find("#ctl00_cphPrincipal_txtUserNamePasswordLast").should("be.visible").type("Hola")
+        cy.iframe().find("#ctl00_cphPrincipal_txtUserNamePasswordLast1").should("be.visible").type("Hola")
         cy.iframe().find("#ctl00_cphPrincipal_txtUserNamePassword").should("be.visible").type("adios")
         cy.iframe().find("#ctl00_cphPrincipal_Button1").should("be.visible").click()
         cy.iframe().wait(1500)
