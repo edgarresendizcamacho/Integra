5c60486248f08ae6cfe44debb09d0a77983d8498
EDGAR <edgarresendiz@hotmail.com>
2021-10-13 14:12:24 -0500

    Proceso de nomina nomina

--- cypress/integration/02 Nomina/02_procesarNomina.js
+++ cypress/integration/02 Nomina/02_procesarNomina.js
@@ -5,12 +5,26 @@ import 'cypress-file-upload';
 require('cypress-xpath')
 require('cypress-iframe')
 
-describe("Validando carga masiva incidencias", () =>{
+describe("Proceso de nomina", () =>{
     
-    it.only("Carga masiva incidencias", () =>{
+    it.only("Nomina servicios quincenal normal", () =>{
         
         cy.login("https://integras3qa.lennken.com/Default.aspx", "0000333", "$0000333$")
         cy.Proceso_Nomina("SERVICIOS , S.A. DE C.V.", "Nómina Quincenal", 1000)
+        
+    })
+
+    it.only("Nomina servicios quincenal asimilada", () =>{
+        
+        cy.login("https://integras3qa.lennken.com/Default.aspx", "0000333", "$0000333$")
+        cy.Proceso_Nomina("SERVICIOS ADMINISTRATIVOS , S.A. DE C.V.", "Nómina Asimilados", 1000)
+        
+    })
+
+    it.only("Nomina servicios quincenal confidencial", () =>{
+        
+        cy.login("https://integras3qa.lennken.com/Default.aspx", "0000333", "$0000333$")
+        cy.Proceso_Nomina("SERVICIOS ADMINISTRATIVOS , S.A. DE C.V.", "Nómina Confidencial", 1000)
         
     })
 
